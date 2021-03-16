993797c2312e6b901c4773c5952bf0e48273b2bd
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-03-16 11:21:51 +0800

    master Write basic test for the HelloWorld contract

---
+++ test/helloworld.js
@@ -0,0 +1,10 @@
+const HelloWorld = artifacts.require('HelloWorld')
+
+contract('HelloWorld', (accounts) => {
+  it('should return my name', async () => {
+      const instance = await HelloWorld.deployed()
+      const value = await instance.getMyName.call()
+
+      assert.equal(value, 'Adam')
+  })
+})
